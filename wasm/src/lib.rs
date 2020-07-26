0a91979edd4f8348c2164f70bfa3eba0ba1285d7
Stevan Goode <steve@madhouseminers.com>
2020-07-26 20:32:19 +0100

    Added food, growth, game over

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,6 +4,12 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = Math)]
+    fn random() -> f32;
+}
+
+#[wasm_bindgen]
 pub enum Direction {
     UP,
     DOWN,
@@ -11,6 +17,7 @@ pub enum Direction {
     RIGHT,
 }
 
+#[derive(Copy, Clone)]
 struct Position {
     x: usize,
     y: usize,
@@ -19,10 +26,11 @@ struct Position {
 #[derive(Copy, Clone)]
 enum ObjectType {
     SPACE,
-    SNAKE_BODY,
-    SNAKE_HEAD,
+    BODY,
+    HEAD,
     FOOD,
     WALL,
+    BORDER,
 }
 
 #[wasm_bindgen]
@@ -30,8 +38,10 @@ pub struct Game {
     board: Vec<Vec<ObjectType>>,
     canvas: web_sys::CanvasRenderingContext2d,
     position: Position,
+    tail: Vec<Position>,
     direction: Direction,
-    doTick: bool,
+    do_tick: bool,
+    length: usize,
 }
 
 #[wasm_bindgen]
@@ -50,29 +60,52 @@ impl Game {
 
         Game {
             canvas: board,
-            position: Position { x: 10, y: 10 },
+            position: Position {
+                x: (canvas.width() / 10) as usize,
+                y: (canvas.height() / 10) as usize,
+            },
             direction: Direction::DOWN,
             board: vec![
                 vec![ObjectType::SPACE; (canvas.width() / 5) as usize];
                 (canvas.height() / 5) as usize
             ],
-            doTick: true,
+            do_tick: true,
+            length: 2,
+            tail: Vec::with_capacity(0),
         }
     }
 
     #[wasm_bindgen]
-    pub fn set_board(self: &mut Game) {
+    pub fn get_do_tick(self: &Game) -> JsValue {
+        wasm_bindgen::JsValue::from(self.do_tick)
+    }
+
+    #[wasm_bindgen]
+    pub fn set_board(self: &mut Game, place_food: bool) {
+        // Resets the game
         self.board = vec![
             vec![ObjectType::SPACE; self.board[0].iter().count()];
             self.board.iter().count() as usize
         ];
+        self.position = Position {
+            x: self.board[0].iter().count() / 2,
+            y: self.board.iter().count() / 2,
+        };
+        self.do_tick = true;
+        self.direction = Direction::DOWN;
+        self.length = 2;
+        self.tail = Vec::with_capacity(0);
+        if place_food == true {
+            self.place_food();
+        }
 
-        self.fill_row(0, ObjectType::WALL);
-        self.fill_row(self.board.iter().count() - 1, ObjectType::WALL);
-        self.fill_col(0, ObjectType::WALL);
-        self.fill_col(self.board[0].iter().count() - 1, ObjectType::WALL);
-
-        self.board[self.position.y][self.position.x] = ObjectType::SNAKE_HEAD;
+        // Draws the board
+        self.fill_row(0, ObjectType::BORDER);
+        self.fill_row(self.board.iter().count() - 1, ObjectType::BORDER);
+        self.fill_col(0, ObjectType::BORDER);
+        self.fill_col(self.board[0].iter().count() - 1, ObjectType::BORDER);
+        self.board[self.position.y][self.position.x] = ObjectType::HEAD;
+        self.render_board();
     }
 
     #[wasm_bindgen]
@@ -88,7 +121,7 @@ impl Game {
 
     #[wasm_bindgen]
     pub fn tick(self: &mut Game) {
-        if self.doTick == true {
+        if self.do_tick == true {
             self.move_snake();
             self.render_board();
         }
@@ -107,8 +140,11 @@ impl Game {
             self.board[i][col_index] = object;
         }
     }
+
     fn move_snake(self: &mut Game) {
+        let initial_position = self.position;
         self.board[self.position.y][self.position.x] = ObjectType::SPACE;
+
         match self.direction {
             Direction::DOWN => self.position.y = self.position.y + 1,
             Direction::UP => self.position.y = self.position.y - 1,
@@ -117,14 +153,30 @@ impl Game {
         }
 
         match self.board[self.position.y][self.position.x] {
-            ObjectType::SPACE => {
-                self.board[self.position.y][self.position.x] = ObjectType::SNAKE_HEAD
-            }
+            ObjectType::SPACE => self.board[self.position.y][self.position.x] = ObjectType::HEAD,
             ObjectType::FOOD => {
-                self.board[self.position.y][self.position.x] = ObjectType::SNAKE_HEAD
+                self.board[self.position.y][self.position.x] = ObjectType::HEAD;
+                self.length = self.length + 1;
+                self.place_food();
             }
-            _ => self.doTick = false,
+            _ => self.do_tick = false,
         }
+
+        if self.tail.iter().count() >= self.length {
+            self.board[self.tail[0].y][self.tail[0].x] = ObjectType::SPACE;
+            self.tail.remove(0);
+        }
+        self.tail.push(initial_position);
+
+        for pos in self.tail.iter() {
+            self.board[pos.y][pos.x] = ObjectType::BODY;
+        }
+    }
+
+    fn place_food(self: &mut Game) {
+        let x = (random() * (self.board[0].iter().count() - 1) as f32) as usize;
+        let y = (random() * (self.board.iter().count() - 1) as f32) as usize;
+        self.board[y][x] = ObjectType::FOOD;
     }
 
     fn render_board(self: &Game) {
@@ -142,15 +194,38 @@ impl Game {
                         self.canvas
                             .set_fill_style(&wasm_bindgen::JsValue::from_str("#000000"));
                         self.canvas
+                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 4.0, 4.0)
+                    }
+                    ObjectType::BORDER => {
+                        self.canvas
+                            .set_fill_style(&wasm_bindgen::JsValue::from_str("#000000"));
+                        self.canvas
                             .fill_rect((cell * 5) as f64, (row * 5) as f64, 5.0, 5.0)
                     }
-                    ObjectType::SNAKE_HEAD => {
+                    ObjectType::HEAD => {
                         self.canvas
                             .set_fill_style(&wasm_bindgen::JsValue::from_str("#FF0000"));
                         self.canvas
-                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 5.0, 5.0)
+                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 4.0, 4.0)
                     }
-                    _ => {}
+                    ObjectType::BODY => {
+                        self.canvas
+                            .set_fill_style(&wasm_bindgen::JsValue::from_str("#FF00FF"));
+                        self.canvas
+                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 4.0, 4.0)
+                    }
+                    ObjectType::FOOD => {
+                        self.canvas
+                            .set_fill_style(&wasm_bindgen::JsValue::from_str("#0000FF"));
+                        self.canvas
+                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 4.0, 4.0)
+                    }
+                    ObjectType::SPACE => {
+                        self.canvas
+                            .set_fill_style(&wasm_bindgen::JsValue::from_str("#dddddd"));
+                        self.canvas
+                            .fill_rect((cell * 5) as f64, (row * 5) as f64, 4.0, 4.0)
+                    }
                 }
             }
         }
--- www/index.js
+++ www/index.js
@@ -1,12 +1,31 @@
 import * as wasm from "../wasm/pkg/wasm";
 
+let gameLoop;
 let game = new wasm.Game(document.getElementById("board"));
-wasm.Game.set_board(game);
+wasm.Game.set_board(game, false);
+document.addEventListener("keyup", (event) => {
+  if (
+    ["ArrowLeft", "ArrowDown", "ArrowRight", "ArrowUp"].indexOf(event.code) > -1
+  ) {
+    if (!gameLoop) playGame();
+    wasm.Game.key_press(game, event.code);
+  }
+});
 
-setInterval(() => {
-  wasm.Game.tick(game);
-}, 15);
+function playGame() {
+  if (gameLoop) return;
 
-document.addEventListener("keyup", (event) =>
-  wasm.Game.key_press(game, event.code)
-);
+  wasm.Game.set_board(game, true);
+  gameLoop = setInterval(() => {
+    wasm.Game.tick(game);
+    if (!wasm.Game.get_do_tick(game)) {
+      clearInterval(gameLoop);
+      gameLoop = null;
+      gameOver();
+    }
+  }, 120);
+}
+
+function gameOver() {
+  alert("Game over");
+}
